5badff56ba53fa70cc38f0902615658fd0fdb64f
dorjizangpo067 <dorjizangpo067@gmail.com>
2026-01-12 12:46:08 +0600

    :bug: only admin can add catagory

--- app/routers/catagory.py
+++ app/routers/catagory.py
@@ -3,7 +3,7 @@ from sqlmodel import Session, select
 from typing import Annotated
 
 from ..models.models import Category
-from ..dependencies import get_session
+from ..dependencies import get_session, admin_role_dependency
 from ..schemas.catagory import CategoryBaseSchema
 
 router = APIRouter(
@@ -14,7 +14,9 @@ router = APIRouter(
 @router.post("/create", response_model=CategoryBaseSchema)
 async def create_category(
     category: CategoryBaseSchema,
-    session: Annotated[Session, Depends(get_session)]
+
+    session: Annotated[Session, Depends(get_session)],
+    admn = Depends(admin_role_dependency)
     ):
     db_category = Category(**category.model_dump(exclude_unset=True))
     session.add(db_category)
